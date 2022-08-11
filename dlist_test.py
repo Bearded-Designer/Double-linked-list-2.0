1517560a02ba6272ffbdc6fa87db41133358a32e
Bearded-Designer <joshsapienza97@gmail.com>
2022-08-11 12:59:29 -0700

    Update dlist_test.py

--- dlist_test.py
+++ dlist_test.py
@@ -1,12 +1,18 @@
 import pytest
 from Double_Linked import Double_Linked
 
-dl = Double_Linked()
+
+@pytest.fixture
+def dl():
+    return dl()
 
 
 def test_answer():
-    assert dl.insert_before(13)
-    assert dl.insert_after(dl.head.next, 12)
+    dl = Double_Linked()
+    dl.insert_before(13)
+    dl.insert_after(dl.head.next, 6)
+    dl.printList(dl.head)
+    assert dl.head.data == 13
 
 
 def f():
